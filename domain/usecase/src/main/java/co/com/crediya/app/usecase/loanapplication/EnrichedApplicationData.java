000c5c1ce66313292129bdad7b7ebc3fdc7dd761
u201923998 (Pacheco Alanya, Jesus Emerson) <jesuspacheco0219@gmail.com>
2025-09-07 21:58:23 -0500

    update logic for retrieve and  valdiate users

--- domain/usecase/src/main/java/co/com/crediya/app/usecase/loanapplication/EnrichedApplicationData.java
+++ domain/usecase/src/main/java/co/com/crediya/app/usecase/loanapplication/EnrichedApplicationData.java
@@ -1,2 +1,14 @@
-package co.com.crediya.app.usecase.loanapplication;public class EnrichedApplicationData {
+package co.com.crediya.app.usecase.loanapplication;
+import co.com.crediya.app.model.loanapplication.LoanApplication;
+import co.com.crediya.app.model.loantype.LoanType;
+import co.com.crediya.app.model.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class EnrichedApplicationData {
+    private final LoanApplication application;
+    private final User user;
+    private final LoanType loanType;
 }
